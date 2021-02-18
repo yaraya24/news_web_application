6e71a90e93cb99a5eaef0ca0c6a8a15282f454f4
Yonnas Araya <yonnas.a24@hotmail.com>
2021-02-18 20:13:41 +1100

    added likes and like count to API endpoint

--- news/models.py
+++ news/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 class NewsOrganisation(models.Model):
 
     name = models.CharField(max_length=50)
--- news/serializers.py
+++ news/serializers.py
@@ -1,14 +1,29 @@
 from rest_framework import serializers
 from .models import NewsArticle
 
+
+
 class ArticleSerializer(serializers.ModelSerializer):
     liked_count = serializers.SerializerMethodField()
+    liked_by_user = serializers.SerializerMethodField()
 
     class Meta:       
-        fields = ('liked_count', 'id', 'news_organisation', 'article_address', 'heading', 'snippet', 'published_date', 'author', 'image_source', 'category')
+        fields = ('liked_by_user','liked_count', 'id', 'news_organisation', 'article_address', 'heading', 'snippet', 'published_date', 'author', 'image_source', 'category')
         model = NewsArticle
         
     def get_liked_count(self, obj):
         qset = obj.likes.count()
+        
         return qset
+
+    def get_liked_by_user(self, obj):
+        request = self.context.get('request', None)
+        if request:
+            user = request.user
+            if obj.likes.filter(id=user.id).first():
+                return user.username
+            else:
+                return False
+        else:
+            return 'None'
    
